117475bb54f4ac5f34e603a23bb8c7b5244439af
Santhosh Hegde <santhosh.hegde262000@gmail.com>
2024-02-26 20:50:29 +0530

    added options to select

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let select = document.querySelector('select');
+let select = document.querySelectorAll('select');
+for (let dropdown of select){
+    for (let CurrCode in countryList){
+        let option= document.createElement('option');
+        option.innerText = CurrCode;
+        option.value = CurrCode;
+        if(dropdown.name=='from' && CurrCode==='USD'){
+            option.selected = 'selected';
+        }
+        if(dropdown.name=='to' && CurrCode==='INR'){
+            option.selected = 'selected';
+        }
+        dropdown.append(option);
+    } 
+    dropdown.addEventListener('change',(evt)=>{
+        console.log("changed",evt)
+    })
+}
